9ca16fd4620f9229a607403c1ab66d1150c65bd2
Angel David Rojas Riveros <halkdavid190@gmail.com>
2024-12-31 19:22:52 -0500

    no crear usuario default

--- src/main/java/com/challenge/foroalura/ForoaluraApplication.java
+++ src/main/java/com/challenge/foroalura/ForoaluraApplication.java
@@ -15,13 +15,4 @@ public class ForoaluraApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ForoaluraApplication.class, args);
 	}
-
-	@Bean
-	public CommandLineRunner run(UsuarioService usuarioService) {
-		return args -> {
-			// Crear usuario cuando la aplicación se inicie
-			usuarioService.crearUsuario("usuarioPrueba", "123456", "usuario@correo.com", Perfil.ADMIN);
-		};
-	}
-
 }
